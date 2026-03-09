1a2e597e7529cba8bf1ce2a6a18dd6a71c531977
tabGourmet <tab_2026@outlook.com>
2026-03-09 16:53:32 -0300

    Add files via upload

---
+++ backend/dist/verify-api.d.ts
@@ -0,0 +1,4 @@
+declare const BASE_URL = "http://127.0.0.1:3001";
+declare const API_URL = "http://127.0.0.1:3001/api/v1";
+declare function runTest(): Promise<void>;
+//# sourceMappingURL=verify-api.d.ts.map
